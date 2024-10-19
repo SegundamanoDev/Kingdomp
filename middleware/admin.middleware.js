f82df4dafab03e53b071011d6d0553f947e4f419
unknown <akitikorisegun2031@gmail.com>
2024-10-19 21:45:33 +0100

    initializing fintech app server

--- middleware/admin.middleware.js
+++ middleware/admin.middleware.js
@@ -2,25 +2,15 @@ const jwt = require('jsonwebtoken');
 const  User  = require('../models/user.model');
 
 const adminMiddleware = async (req, res, next) => {
-  const token = req.header('Authorization')?.split(' ')[1];
-
-  if (!token) {
-    return res.status(401).json({ message: 'No token, authorization denied' });
-  }
-
   try {
-    // Decode the token
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-    // Find the user by userId
-    const user = await User.findByPk(decoded.userId);
+   const user = req.user
 
     if (!user) {
       return res.status(401).json({ message: 'User not found' });
     }
     
     if(!user.isAdmin){
-        return res.status(401).json({ message: 'Only an admin can access this route' })
+        return res.status(401).json({ message: 'Admin access required' })
     }
     // Continue to the next middleware
     next();
